3a02cc6c31979832a58a8b16273ac89b08667c49
MirkOrlando <mirko@orlando-m.it>
2022-06-29 15:00:44 +0200

    create API posts

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('posts', function () {
+    $posts = Post::with(['tags', 'category', 'user'])->paginate(9);
+
+    return $posts;
 });
